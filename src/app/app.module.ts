7d667cef4cbe637373fe0fd99d5b8a2c2670f324
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-09-19 02:39:26 +0530

    [Vaibhav]REFACTOR: use common header component for welcome message and top bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { WelcomeComponent } from './components/welcome/welcome.component';
 import { ConversionComponent } from './components/conversion/conversion.component';
 import { ChooseQuantityComponent } from './components/choose-quantity/choose-quantity.component';
 import { CommonModule } from '@angular/common';
@@ -13,7 +12,6 @@ import { CommonModule } from '@angular/common';
   declarations: [
     AppComponent,
     HeaderComponent,
-    WelcomeComponent,
     ConversionComponent,
     ChooseQuantityComponent
    ],
