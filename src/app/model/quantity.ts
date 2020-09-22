71e39f8bdec98c8f9a29fec728ed62704972e8e8
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-09-22 18:53:52 +0530

    [Vaibhav]ADD: quantity model for passing data in json format

---
+++ src/app/model/quantity.ts
@@ -0,0 +1,23 @@
+export class quantity {
+    firstSubUnit: string
+    quantity: number
+    secondSubUnit: string
+
+    constructor(firstSubUnit,quantity,secondSubUnit) {
+        this.firstSubUnit = firstSubUnit
+        this.quantity = quantity
+        this.secondSubUnit = secondSubUnit
+    }
+
+    set setFirstSubUnit(firstSubUnit: string){
+        this.firstSubUnit = firstSubUnit
+    }
+
+    set setQuantity(quantity: number){
+        this.quantity = quantity
+    }
+
+    set setSecondSubUnit(secondSubUnit: string){
+        this.secondSubUnit = secondSubUnit
+    }
+}
